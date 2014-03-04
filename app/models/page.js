0e5e2def85f0968d3ac7f202c2aa3ac9e319534a
dbaines <dbaines@gmail.com>
2014-03-04 17:30:02 +1030

    Added pages and content

--- app/models/page.js
+++ app/models/page.js
@@ -2,36 +2,102 @@ var Page =  DS.Model.extend({
   title:    DS.attr('string'),
   parent:   DS.belongsTo('page'),
   children: DS.hasMany('page', { async: true }),
+
   path: function() {
     var parent = this.get('parent');
     var path = "";
-
     if (parent !== null) {
       path +=  parent.get('path') + "/";
     }
-
     return path + this.get('id');
+  }.property('id'),
+
+  navImage: function(){
+    return "images/nav/" + this.get('id') + ".png";
   }.property('id')
+
 });
 
 Page.FIXTURES = [
   {
-    id: 'home-page',
-    title: 'Home Page',
-    children: ['child-page']
-  },
-  {
-    id: 'child-page',
-    title: 'Child Page',
-    parent: 'home-page',
-    children: ['child-child-page']
-  },
-  {
-    id: 'child-child-page',
-    title: 'Child Child Page',
-    parent: 'child-page',
+    id: "about-us",
+    title: "About Us",
     children: []
   },
+  {
+    id: "our-people",
+    title: "Our People",
+    children: []
+  },
+  {
+    id: "services",
+    title: "Services",
+    children: ["workers-compensation", "disability-employment-services", "supported-wage-system"]
+  },
+  {
+    id: "quality",
+    title: "Quality",
+    children: []
+  },
+  {
+    id: "careers",
+    title: "Careers",
+    children: []
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    children: []
+  },
+  {
+    id: "referrals",
+    title: "Referrals",
+    children: []
+  },
+  // services sub-pages
+  {
+    id: "workers-compensation",
+    title: "Workers Compensation",
+    children: ["return-to-work", "intensive-job-seeking", "community-restoration", "vocational-and-suitable-employment-assessments"],
+    parent: "services"
+  },
+  {
+    id: "disability-employment-services",
+    title: "Disability Employment Services",
+    children: [],
+    parent: "services"
+  },
+  {
+    id: "supported-wage-system",
+    title: "Supported Wage System &amp; On-going Support Assessments",
+    children: [],
+    parent: "services"
+  },
+  // workers comp sub-pages
+  {
+    id: "return-to-work",
+    title: "Return To Work",
+    children: [],
+    parent: 'workers-compensation'
+  },
+  {
+    id: "intensive-job-seeking",
+    title: "Intensive Job Seeking",
+    children: [],
+    parent: 'workers-compensation'
+  },
+  {
+    id: "community-restoration",
+    title: "Community Restoration",
+    children: [],
+    parent: 'workers-compensation'
+  },
+  {
+    id: "vocational-and-suitable-employment-assessments",
+    title: "Vocational and Suitable Employment Assessments",
+    children: [],
+    parent: 'workers-compensation'
+  }
 ];
 
 export default Page;
