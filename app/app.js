ccee706832b0242a2c47541439e59b5e064e3997
dbaines <dbaines@gmail.com>
2014-03-06 16:54:31 +1030

    Split our-people pages out

--- app/app.js
+++ app/app.js
@@ -11,17 +11,17 @@ var App = Ember.Application.extend({
 });
 
 // fix ember data inspector
-DS.DebugAdapter.reopen({
-  getModelTypes: function() {
-    var self = this;
-    return Ember.keys(requirejs._eak_seen).filter(function(key) {
-      return !!key.match(/^appkit\/models\//) && self.detect(require(key).default);
-    }).map(function(key){
-      var type = require(key).default, typeKey = key.match(/^appkit\/models\/(.*)/)[1];
-      type.toString = function() { return typeKey; };
-      return type;
-    });
-  }
-});
+// DS.DebugAdapter.reopen({
+//   getModelTypes: function() {
+//     var self = this;
+//     return Ember.keys(requirejs._eak_seen).filter(function(key) {
+//       return !!key.match(/^appkit\/models\//) && self.detect(require(key).default);
+//     }).map(function(key){
+//       var type = require(key).default, typeKey = key.match(/^appkit\/models\/(.*)/)[1];
+//       type.toString = function() { return typeKey; };
+//       return type;
+//     });
+//   }
+// });
 
 export default App;
